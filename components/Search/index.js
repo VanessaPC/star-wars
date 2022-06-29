212a4d87a48c0fd2aa8023df1900743854ea7659
w1kman <thomas@w1kman.se>
2022-06-29 08:49:37 +0200

    [fix] Add `dashboardImage` toggle based on theme - `starwars-light.jpg` will now be used when light theme (`id: "01"`) is selected

--- components/Search/index.js
+++ components/Search/index.js
@@ -1,19 +1,27 @@
+import { ThemeContext } from "styled-components"
 import { HeroImage } from "../HeroImage";
-import dashboardImage from "../../styles/img/starwars.jpg";
+import dashboardImageDark from "../../styles/img/starwars.jpg";
+import dashboardImageLight from '../../styles/img/starwars-light.jpg'
 import { SearchContainer, SearchInput, ContainerInput } from "./styles";
+import { useContext } from 'react'
 
-export const Search = ({ setSearchTerm, searchTerm }) => (
-  <SearchContainer>
-    <HeroImage src={dashboardImage} alt="" />
-    <ContainerInput>
-      <SearchInput
-        type="text"
-        minLength={1}
-        value={searchTerm}
-        placeholder="Search your Star Wars character"
-        debounceTimeout={500}
-        onChange={(e) => setSearchTerm(e.target.value.trim())}
-      />
-    </ContainerInput>
-  </SearchContainer>
-);
+export const Search = ({ setSearchTerm, searchTerm }) => {
+  const theme = useContext(ThemeContext)
+  const dashboardImage = (theme.id === "01") ? dashboardImageLight : dashboardImageDark
+
+  return (
+    <SearchContainer>
+      <HeroImage src={dashboardImage} alt="" />
+      <ContainerInput>
+        <SearchInput
+          type="text"
+          minLength={1}
+          value={searchTerm}
+          placeholder="Search your Star Wars character"
+          debounceTimeout={500}
+          onChange={(e) => setSearchTerm(e.target.value.trim())}
+        />
+      </ContainerInput>
+    </SearchContainer>
+  );
+}
